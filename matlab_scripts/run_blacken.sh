7fc7d977a7a14b3e22145428ef287c64b1861739
amatveev <amatveev@csail.mit.edu>
2016-06-16 14:27:04 -0400

    delete all

--- matlab_scripts/run_blacken.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo matlab -nodisplay -nojvm  -r "blacken_block $1; quit;"
-
-matlab -nodisplay -nojvm  -r "blacken_block $1; quit;"
--- watershed/coordinates_conversion.h
+++
@@ -1,9 +0,0 @@
-#ifndef __COORDINATES_CONVERSION__
-#define __COORDINATES_CONVERSION__
-
-#define XYZ_TO_INDEX(X, Y, Z) (Z*(rows*cols) + Y*cols + X)
-#define INDEX_TO_Z(INDEX) (INDEX/(rows*cols))
-#define INDEX_TO_Y(INDEX) ((INDEX % (rows*cols))/cols)
-#define INDEX_TO_X(INDEX) (INDEX%cols)
-
-#endif //__COORDINATES_CONVERSION__
