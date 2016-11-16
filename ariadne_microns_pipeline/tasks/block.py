d8db3753f23d419bae78540c5262357b453fcd4e
Lee Kamentsky <lee_kamentsky@g.harvard.edu>
2016-11-16 14:19:27 -0500

    Added ability to downsample and map segmentations in BlockTask

--- ariadne_microns_pipeline/tasks/block.py
+++ ariadne_microns_pipeline/tasks/block.py
@@ -4,6 +4,9 @@
 
 import json
 import luigi
+import numpy as np
+from scipy.ndimage import map_coordinates
+from .connected_components import ConnectivityGraph
 from .utilities import RequiresMixin, RunMixin
 from ..targets.factory import TargetFactory
 from ..parameters import DatasetLocationParameter, VolumeParameter
@@ -19,6 +22,21 @@ class BlockTaskMixin:
     input_volumes = MultiVolumeParameter(
         description="The volumes that will be composited to form the output "
         "volume.")
+    downsample_xy = luigi.FloatParameter(
+        default=1.0,
+        description="# of input voxels in the x and y direction that become "
+        "one voxel in the output volume")
+    downsample_z = luigi.FloatParameter(
+        default=1.0,
+        description="# of input voxels in the z direction that become one "
+        "voxel in the output volume")
+    wants_nearest = luigi.BoolParameter(
+        description="Instead of interpolating, take the value of the nearest "
+                    "pixel (e.g. for segmentation data)")
+    mapping = luigi.Parameter(
+        default="/dev/null",
+        description="A local to global segmentation mapping produced by "
+                    "the AllConnectedComponents task.")
     def input(self):
         '''Return the volumes to be assembled'''
         tf = TargetFactory()
@@ -47,31 +65,84 @@ class BlockTaskRunMixin:
         #        size of the blocks being written.
         output_volume = self.output()
         first = True
+        #
+        # The downsampled output coordinates
+        #
+        x0d = self.output_volume.x
+        x1d = self.output_volume.x1
+        y0d = self.output_volume.y
+        y1d = self.output_volume.y1
+        z0d = self.output_volume.z
+        z1d = self.output_volume.z1
+        if self.mapping == "/dev/null":
+            mapping = None
+        else:
+            mapping = ConnectivityGraph.load(open(self.mapping, "r"))
+            
         for input_volume in self.input():
             #
             # Compute the portion of the input volume that overlaps
             # with the requested output volume.
             #
-            x0 = max(self.output_volume.x, input_volume.x)
-            x1 = min(self.output_volume.x1, input_volume.x + input_volume.width)
+            x0 = int(max(x0d, 
+                         np.floor(input_volume.volume.x / self.downsample_xy)))
+            x1 = int(min(x1d, 
+                         np.ceil(input_volume.volume.x1 / self.downsample_xy)))
             if x0 >= x1:
                 continue
-            y0 = max(self.output_volume.y, input_volume.y)
-            y1 = min(self.output_volume.y1, 
-                     input_volume.y + input_volume.height)
+            y0 = int(max(y0d, 
+                         np.floor(input_volume.volume.y / self.downsample_xy)))
+            y1 = int(min(y1d, 
+                         np.ceil(input_volume.volume.y1 / self.downsample_xy)))
             if y0 >= y1:
                 continue
-            z0 = max(self.output_volume.z, input_volume.z)
-            z1 = min(self.output_volume.z1, input_volume.z + input_volume.depth)
+            z0 = int(max(z0d, 
+                         np.floor(input_volume.volume.z / self.downsample_z)))
+            z1 = int(min(z1d, 
+                         np.ceil(input_volume.volume.z1 / self.downsample_z)))
             if z0 >= z1:
                 continue
-            subvolume = input_volume.imread_part(
-                x0, y0, z0, x1-x0, y1-y0, z1-z0)
+            if self.downsample_xy == 1.0 and self.downsample_z == 1.0:
+                subvolume = input_volume.imread_part(
+                    x0, y0, z0, x1-x0, y1-y0, z1-z0)
+            else:
+                x0i = x0 * self.downsample_xy
+                x0ii = int(x0i)
+                x1i = x1 * self.downsample_xy
+                x1ii = int(np.ceil(x1i))
+                y0i = y0 * self.downsample_xy
+                y0ii = int(y0i)
+                y1i = y1 * self.downsample_xy
+                y1ii = int(np.ceil(y1i))
+                z0i = z0 * self.downsample_z
+                z0ii = int(z0)
+                z1i = z1 * self.downsample_z
+                z1ii = int(np.ceil(z1i))
+                x = np.linspace(x0i, x1i, (x1 - x0), endpoint=False)\
+                    [np.newaxis, np.newaxis, :]
+                y = np.linspace(y0i, y1i, (y1 - y0), endpoint=False)\
+                    [np.newaxis, :, np.newaxis]
+                z = np.linspace(z0i, z1i, (z1 - z0), endpoint=False)\
+                    [:, np.newaxis, np.newaxis]
+                base = np.zeros(
+                    (z.shape[0], y.shape[1], x.shape[2]), np.float32)
+                coords = [base + (z  - z0ii).astype(np.float32),
+                          base + (y - y0ii).astype(np.float32),
+                          base + (x - x0ii).astype(np.float32)]
+                subvolume = input_volume.imread_part(
+                    x0ii, y0ii, z0ii, x1ii - x0ii, y1ii - y0ii, z1ii - z0ii)
+                if self.wants_nearest:
+                    order = 0
+                else:
+                    order = 3
+                subvolume = map_coordinates(subvolume, coords, order=order)
+            if mapping is not None:
+                subvolume = mapping.convert(subvolume, input_volume.volume)
             if first:
                 first = False
                 output_volume.create_volume(subvolume.dtype)
-                
             output_volume.imwrite_part(subvolume, x0, y0, z0)
+                
         output_volume.finish_volume()
 
 
