2b9005fde73ba4c2b0ef068dc5761bed42fb8b80
Lee Kamentsky <lee_kamentsky@g.harvard.edu>
2016-11-23 09:34:47 -0500

    NeuTu skeletonization options

--- ariadne_microns_pipeline/tasks/skeletonize.py
+++ ariadne_microns_pipeline/tasks/skeletonize.py
@@ -45,8 +45,37 @@ class SkeletonizeRunMixin:
         default=0.5,
         description="Prune leaves if they are less than this fraction of "
                     "their parents' volume")
+    use_neutu = luigi.BoolParameter(
+        description="Use NeuTu's skeletonizer")
+    downsampling_factor = luigi.IntParameter(
+        default=0,
+        description="Downsample image by this factor to reduce processing time")
+    minimal_length = luigi.FloatParameter(
+        default=50.0,
+        description="Neutu: minimum length of a branch")
+    keep_single_object = luigi.BoolParameter(
+        description="Neutu: keep an object even if it would otherwise be filtered by size")
+    rebase = luigi.BoolParameter(
+        description="Neutu: reset the starting point to a terminal point")
+    filling_hole = luigi.BoolParameter(
+        description="Neutu: Fill holes in the object before skeletonizing")
+    maximal_distance = luigi.FloatParameter(
+        default=-1.0,
+        description="Neutu: maximum distance to connect isolated branches")
+    minimal_object_size = luigi.IntParameter(
+        default=0,
+        description="Neutu: minimum area for an object before removing.")
+    skeletonize_stack_location = luigi.Parameter(
+        default="/dev/null",
+        description="Location of the NeuTu skeletonize_stack binary")
     
     def ariadne_run(self):
+        if self.use_neutu:
+            self.skeletonize_using_neutu()
+        else:
+            self.skeletonize_using_microns()
+    
+    def skeletonize_using_microns(self):
         seg = self.input().next().imread()
         result = skeletonize(seg, self.xy_nm, self.z_nm, self.decimation_factor,
                     self.cpu_count)
@@ -59,6 +88,98 @@ class SkeletonizeRunMixin:
             paths.append(path)
         with self.output().open("w") as fd:
             json.dump(paths, fd)
+    
+    def skeletonize_using_neutu(self):
+        '''Skeletonize by running "skeletonize_stack" to execute NeuTu's code
+        
+        See https://github.com/vcg/microns_skeletonization
+        '''
+        #
+        # skeletonize_stack takes a single JSON file that tells it what
+        # to do. Here's the schema
+        #
+        # volume:
+        #   x: x coordinate of the volume's origin
+        #   y: y coordinate of the volume's origin
+        #   z: z coordinate of the volume's origin
+        #   width: width of the volume in voxels
+        #   height: height of the volume in voxels
+        #   depth: depth of the volume in voxels
+        #   xy_nm: length of a voxel in the x and y directions in nanometers
+        #   z_nm: length of a voxel in the z direction in nanometers
+        #   planes: array of strings of the filenames of each plane.
+        # processing_instructions:
+        #   See ./neurolabi/json/skeletonize.schema.json in the NeuTu project
+        # output:
+        #   Dictionary of label to .swc file to write
+        #
+        seg_target = self.input().next()
+        #
+        # Find the labels we should write
+        #
+        seg = seg_target.imread()
+        areas = np.bincount(seg.ravel())
+        areas[0] = 0
+        labels = np.where(areas > 0)[0]
+        #
+        # Build the input volume
+        #
+        volume = dict(x=self.volume.x,
+                      y=self.volume.y,
+                      z=self.volume.z,
+                      width=self.volume.width,
+                      height=self.volume.height,
+                      depth=self.volume.depth,
+                      xy_nm=self.xy_nm,
+                      z_nm=self.z_nm,
+                      planes=seg_target.get_filenames())
+        #
+        # Build the processing instructions
+        #
+        downsample_xy = self.downsampling_factor
+        if downsample_xy == 1:
+            # downsampleInterval=0 means "Don't downsample" which is the
+            # intent of the user specifying "1"
+            #
+            downsample_xy = 0
+        processing_instructions = dict(
+            downsampleInterval=[downsample_xy, downsample_xy, 0],
+            minimalLength=self.minimal_length,
+            keepingSingleObject=self.keep_single_object,
+            rebase=self.rebase,
+            fillingHole=self.filling_hole,
+            maximalDistance=self.maximal_distance,
+            minimalObjectSize=self.minimal_object_size)
+        #
+        # Build the output
+        #
+        output_target = self.output()
+        root = os.path.splitext(output_target.path)[0]
+        if not os.path.isdir(root):
+            os.makedirs(root)
+        output = dict(
+            [(label, os.path.join(root, "%d.swc" % label))
+             for label in labels])
+        #
+        # Write the config file to temp storage.
+        #
+        d = dict(volume=volume,
+                 processing_instructions=processing_instructions,
+                 output=output)
+        fileno, filename = tempfile.mkstemp(".json")
+        try:
+            fd = os.fdopen(fileno, "w")
+            json.dump(d, fd)
+            fd.close()
+            #
+            # Call the subprocess with the file
+            #
+            subprocess.check_call([self.skeletonize_stack_location,
+                                   filename])
+            with output_target.open("w") as fd:
+                json.dump(output.values(), fd)
+        finally:
+            os.remove(filename)
 
 class SkeletonizeTask(SkeletonizeTaskMixin,
                       SkeletonizeRunMixin,
