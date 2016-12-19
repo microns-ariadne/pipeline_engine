892fc7ac9bfb665a21f8356f35d96b63c4323a23
Lee Kamentsky <lee_kamentsky@g.harvard.edu>
2016-12-19 15:53:50 -0500

    Fixed some corner cases with stitching

--- ariadne_microns_pipeline/tasks/stitch_segmentation.py
+++ ariadne_microns_pipeline/tasks/stitch_segmentation.py
@@ -6,6 +6,10 @@ import json
 import luigi
 import numpy as np
 import os
+import Queue
+import rh_logger
+import multiprocessing
+import time
 
 from ..parameters import VolumeParameter, DatasetLocationParameter
 from ..parameters import Volume, DatasetLocation
@@ -55,6 +59,15 @@ class StitchSegmentationRunMixin:
     z_chunking = luigi.IntParameter(
         default=4,
         description="The # of voxels in an HDF5 chunk in the z direction")
+    x_padding = luigi.IntParameter(
+        default=0,
+        description="The amount of overlap between volumes in the x direction.")
+    y_padding = luigi.IntParameter(
+        default=0,
+        description="The amount of overlap between volumes in the x direction.")
+    z_padding = luigi.IntParameter(
+        default=0,
+        description="The amount of overlap between volumes in the z direction")
     compression = luigi.EnumParameter(
         enum=Compression,
         default=Compression.GZIP,
@@ -62,12 +75,6 @@ class StitchSegmentationRunMixin:
     
     def ariadne_run(self):
         output_tgt = self.output()
-        x0 = self.output_volume.x
-        y0 = self.output_volume.y
-        z0 = self.output_volume.z
-        x1 = self.output_volume.x1
-        y1 = self.output_volume.y1
-        z1 = self.output_volume.z1
         chunks=(min(self.z_chunking, self.output_volume.depth),
                 min(self.xy_chunking, self.output_volume.height),
                 min(self.xy_chunking, self.output_volume.width))
@@ -76,12 +83,29 @@ class StitchSegmentationRunMixin:
             kwds["compression"] = "gzip"
         elif self.compression == Compression.LZF:
             kwds["compression"] = "lzf"
-        output_tgt.create_volume(dtype=np.uint32, **kwds)
+        queue = multiprocessing.Queue(4)
+        result = multiprocessing.Value("i")
+        result.value = 0
+        worker = multiprocessing.Process(
+            target = writer,
+            args = (queue, output_tgt.h5path, output_tgt.dataset_path,
+                    (output_tgt.depth, output_tgt.height, output_tgt.width),
+                    kwds, result))
+        worker.start()
+        x0 = self.output_volume.x
+        y0 = self.output_volume.y
+        z0 = self.output_volume.z
+        x1 = self.output_volume.x1
+        y1 = self.output_volume.y1
+        z1 = self.output_volume.z1
         inputs = self.input()
         
         cc_location = inputs.next()
+        t0 = time.time()
         cc = json.load(cc_location.open("r"),
                        object_hook = to_hashable)
+        rh_logger.logger.report_metric(
+            "Component graph load time (sec)", time.time() - t0)
         #
         # This is a map of volume to local/global labelings
         #
@@ -109,20 +133,82 @@ class StitchSegmentationRunMixin:
                 volume_map[to_hashable(tgt.volume.to_dictionary())])
             mapping = np.zeros(np.max(tmp[:, 0]) + 1, np.uint32)
             mapping[tmp[:, 0]] = tmp[:, 1]
-            seg = mapping[tgt.imread()]
+            seg = tgt.imread()
+            t0 = time.time()
+            seg = mapping[seg]
+            rh_logger.logger.report_metric("Mapping time (sec)", time.time() - t0)
             
             x0a = max(x0, tgt.volume.x)
+            if x0a != x0:
+                x0a += self.x_padding / 2 - 1
             x1a = min(x1, tgt.volume.x1)
+            if x1a != x1:
+                x1a -= self.x_padding / 2 - 1
             y0a = max(y0, tgt.volume.y)
+            if y0a != y0:
+                y0a += self.y_padding / 2 - 1
             y1a = min(y1, tgt.volume.y1)
+            if y1a != y1:
+                y1a -= self.y_padding / 2 - 1
             z0a = max(z0, tgt.volume.z)
+            if z0a != z0:
+                z0a += self.z_padding / 2 - 1
             z1a = min(z1, tgt.volume.z1)
-            output_tgt.imwrite_part(
+            if z1a != z1:
+                z1a -= self.z_padding / 2 - 1
+            if x1a < x0a or y1a < y0a or z1a < z0a:
+                continue
+            queue.put((
                 seg[z0a-tgt.volume.z:z1a-tgt.volume.z,
                     y0a-tgt.volume.y:y1a-tgt.volume.y,
                     x0a-tgt.volume.x:x1a-tgt.volume.x],
-                x0a, y0a, z0a)
+                x0a-x0, y0a-y0, z0a-z0))
+            if result.value < 0:
+                raise Exception("Writer process failed. See log for details")
+        queue.put((None, None, None, None))
+        queue.close()
+        queue.join_thread()
+        if result.value < 0:
+            raise Exception("Writer process failed. See log for details")
         output_tgt.finish_volume()
+        worker.join()
+
+def writer(queue, hdf_file, dataset_name, shape, create_dataset_kwds, result):
+    '''The writer runs on its own thread, writing blocks from the queue.
+    
+    '''
+    
+    try:
+        rh_logger.logger.start_process(
+            "HDFWriter", "Starting stitching writer", [])
+        rh_logger_started = True
+    except:
+        rh_logger.logger.report_event("Starting stitching writer")
+        rh_logger_started = False
+    try:
+        t0 = time.time()
+        with h5py.File(hdf_file, "w") as fd:
+            rh_logger.logger.report_event(
+                "Creating dataset with shape %s" % repr(shape))
+            ds = fd.create_dataset(dataset_name, shape=shape, dtype=np.uint32, 
+                                   **create_dataset_kwds)
+            rh_logger.logger.report_metric(
+                "HDF5 dataset creation (sec)", time.time() - t0)
+            while True:
+                block, x, y, z = queue.get()
+                if block is None:
+                    break
+                t0 = time.time()
+                ds[z:z+block.shape[0],
+                   y:y+block.shape[1],
+                   x:x+block.shape[2]] = block
+                rh_logger.logger.report_metric("HDF5 write time (sec)", 
+                                               time.time() - t0)
+    except:
+        result.value = -1
+        rh_logger.logger.report_exception()
+    if rh_logger_started:
+        rh_logger.logger.end_process("Stitching writer exiting")
 
 class StitchSegmentationTask(StitchSegmentationTaskMixin,
                              StitchSegmentationRunMixin,
