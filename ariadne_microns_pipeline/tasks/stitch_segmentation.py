214f95664024b02e2df7a162a8fd36696a5fb56f
Lee Kamentsky <lee_kamentsky@g.harvard.edu>
2017-04-10 15:14:24 -0400

    Multiple processes for readers / how about lzf compression?

--- ariadne_microns_pipeline/tasks/stitch_segmentation.py
+++ ariadne_microns_pipeline/tasks/stitch_segmentation.py
@@ -59,8 +59,14 @@ class StitchSegmentationRunMixin:
         description="The amount of overlap between volumes in the z direction")
     compression = luigi.EnumParameter(
         enum=Compression,
-        default=Compression.GZIP,
+        default=Compression.LZF,
         description="The type of compression for the HDF5 file")
+    compression_level = luigi.IntParameter(
+        default=4,
+        description="For GZip, the level of compression (0 = lowest, 9 = most)")
+    n_workers = luigi.IntParameter(
+        default=4,
+        description="Number of reader worker processes")
     
     def ariadne_run(self):
         output_tgt = self.output()
@@ -70,96 +76,119 @@ class StitchSegmentationRunMixin:
         kwds = dict(chunks=chunks)
         if self.compression == Compression.GZIP:
             kwds["compression"] = "gzip"
+            kwds["compression_opts"] = str(self.compression_level)
         elif self.compression == Compression.LZF:
             kwds["compression"] = "lzf"
-        queue = multiprocessing.Queue(4)
         result = multiprocessing.Value("i")
         result.value = 0
-        with output_tgt.open("w") as fd:
-            worker = multiprocessing.Process(
-                target = writer,
-                args = (queue, fd.name, self.dataset_name,
-                    (self.output_volume.depth, 
-                     self.output_volume.height, 
-                     self.output_volume.width),
-                    kwds, result))
-            worker.start()
-            x0 = self.output_volume.x
-            y0 = self.output_volume.y
-            z0 = self.output_volume.z
-            x1 = self.output_volume.x1
-            y1 = self.output_volume.y1
-            z1 = self.output_volume.z1
-            inputs = self.input()
-            
-            cc_location = inputs.next()
-            t0 = time.time()
-            cc = json.load(cc_location.open("r"),
-                           object_hook = to_hashable)
-            rh_logger.logger.report_metric(
-                "Component graph load time (sec)", time.time() - t0)
-            #
-            # This is a map of volume to local/global labelings
-            #
-            volume_map = dict(cc["volumes"])
-            # Get input volumes from connected components
-            #
-            inputs = []
-            for volume, location in cc["locations"]:
-                inputs.append(DestVolumeReader(location))
-            #
-            # Loop over each volume
-            #
-            for tgt in inputs:
-                if tgt.volume.x1 <= x0 or\
-                   tgt.volume.y1 <= y0 or\
-                   tgt.volume.z1 <= z0 or\
-                   tgt.volume.x >= x1 or \
-                   tgt.volume.y >= y1 or \
-                   tgt.volume.z >= z1:
-                    continue
-                tmp = np.array(
-                    volume_map[to_hashable(tgt.volume.to_dictionary())])
-                mapping = np.zeros(np.max(tmp[:, 0]) + 1, np.uint32)
-                mapping[tmp[:, 0]] = tmp[:, 1]
-                seg = tgt.imread()
-                t0 = time.time()
-                seg = mapping[seg]
-                rh_logger.logger.report_metric("Mapping time (sec)", time.time() - t0)
+        pool = multiprocessing.Pool(self.n_workers)
+        with multiprocessing.Manager() as manager:
+            queue = manager.Queue(4)
+            with output_tgt.open("w") as fd:
+                worker = multiprocessing.Process(
+                    target = writer,
+                    args = (queue, fd.name, self.dataset_name,
+                        (self.output_volume.depth, 
+                         self.output_volume.height, 
+                         self.output_volume.width),
+                        kwds, result))
+                worker.start()
+                x0 = self.output_volume.x
+                y0 = self.output_volume.y
+                z0 = self.output_volume.z
+                x1 = self.output_volume.x1
+                y1 = self.output_volume.y1
+                z1 = self.output_volume.z1
+                inputs = self.input()
                 
-                x0a = max(x0, tgt.volume.x)
-                if x0a != x0 and self.x_padding > 0:
-                    x0a += self.x_padding / 2 - 1
-                x1a = min(x1, tgt.volume.x1)
-                if x1a != x1 and self.x_padding > 0:
-                    x1a -= self.x_padding / 2 - 1
-                y0a = max(y0, tgt.volume.y)
-                if y0a != y0 and self.y_padding > 0:
-                    y0a += self.y_padding / 2 - 1
-                y1a = min(y1, tgt.volume.y1)
-                if y1a != y1 and self.y_padding > 0:
-                    y1a -= self.y_padding / 2 - 1
-                z0a = max(z0, tgt.volume.z)
-                if z0a != z0 and self.z_padding > 0:
-                    z0a += self.z_padding / 2 - 1
-                z1a = min(z1, tgt.volume.z1)
-                if z1a != z1 and self.z_padding > 0:
-                    z1a -= self.z_padding / 2 - 1
-                if x1a < x0a or y1a < y0a or z1a < z0a:
-                    continue
-                queue.put((
-                    seg[z0a-tgt.volume.z:z1a-tgt.volume.z,
-                        y0a-tgt.volume.y:y1a-tgt.volume.y,
-                        x0a-tgt.volume.x:x1a-tgt.volume.x],
-                    x0a-x0, y0a-y0, z0a-z0))
+                cc_location = inputs.next()
+                t0 = time.time()
+                cc = json.load(cc_location.open("r"),
+                               object_hook = to_hashable)
+                rh_logger.logger.report_metric(
+                    "Component graph load time (sec)", time.time() - t0)
+                #
+                # This is a map of volume to local/global labelings
+                #
+                volume_map = dict(cc["volumes"])
+                # Get input volumes from connected components
+                #
+                read_results = []
+                for volume, location in cc["locations"]:
+                    rr = pool.apply_async(
+                        read_block, 
+                        args=(location, x0, x1, y0, y1, z0, z1, volume_map, 
+                              queue, self.x_padding, self.y_padding,
+                              self.z_padding))
+                    read_results.append(rr)
+                pool.close()
+                pool.join()
+                for rr in read_results:
+                    rr.get()
                 if result.value < 0:
                     raise Exception("Writer process failed. See log for details")
-            queue.put((None, None, None, None))
-            queue.close()
-            queue.join_thread()
-            if result.value < 0:
-                raise Exception("Writer process failed. See log for details")
-            worker.join()
+                queue.put((None, None, None, None))
+                if result.value < 0:
+                    raise Exception("Writer process failed. See log for details")
+                worker.join()
+
+def read_block(location, x0, x1, y0, y1, z0, z1, volume_map, queue,
+               x_padding, y_padding, z_padding):
+    try:
+        rh_logger.logger.start_process(
+            "BlockReader", "Starting stitching reader", [])
+    except:
+        pass
+    tgt = DestVolumeReader(location)
+    if tgt.volume.x1 <= x0 or\
+       tgt.volume.y1 <= y0 or\
+       tgt.volume.z1 <= z0 or\
+       tgt.volume.x >= x1 or \
+       tgt.volume.y >= y1 or \
+       tgt.volume.z >= z1:
+        rh_logger.logger.report_event(
+        "Skipping %d:%d, %d:%d, %d:%d" %
+        (tgt.volume.x, tgt.volume.x1,
+         tgt.volume.y, tgt.volume.y1,
+         tgt.volume.z, tgt.volume.z1))
+        rh_logger.logger.report_event(
+            "Output volume: %d:%d %d:%d %d:%d" % (x0, x1, y0, y1, z0, z1))
+        return
+    tmp = np.array(
+        volume_map[to_hashable(tgt.volume.to_dictionary())])
+    mapping = np.zeros(np.max(tmp[:, 0]) + 1, np.uint32)
+    mapping[tmp[:, 0]] = tmp[:, 1]
+    seg = tgt.imread()
+    t0 = time.time()
+    seg = mapping[seg]
+    rh_logger.logger.report_metric("Mapping time (sec)", time.time() - t0)
+    
+    x0a = max(x0, tgt.volume.x)
+    if x0a != x0 and x_padding > 0:
+        x0a += x_padding / 2 - 1
+    x1a = min(x1, tgt.volume.x1)
+    if x1a != x1 and x_padding > 0:
+        x1a -= x_padding / 2 - 1
+    y0a = max(y0, tgt.volume.y)
+    if y0a != y0 and y_padding > 0:
+        y0a += y_padding / 2 - 1
+    y1a = min(y1, tgt.volume.y1)
+    if y1a != y1 and y_padding > 0:
+        y1a -= y_padding / 2 - 1
+    z0a = max(z0, tgt.volume.z)
+    if z0a != z0 and z_padding > 0:
+        z0a += z_padding / 2 - 1
+    z1a = min(z1, tgt.volume.z1)
+    if z1a != z1 and z_padding > 0:
+        z1a -= z_padding / 2 - 1
+    if x1a < x0a or y1a < y0a or z1a < z0a:
+        return
+    queue.put((
+        seg[z0a-tgt.volume.z:z1a-tgt.volume.z,
+            y0a-tgt.volume.y:y1a-tgt.volume.y,
+            x0a-tgt.volume.x:x1a-tgt.volume.x],
+        x0a-x0, y0a-y0, z0a-z0))
+    return (x1a - x0a, y1a-y0a, z1a-z0a)
 
 def writer(queue, hdf_file, dataset_name, shape, create_dataset_kwds, result):
     '''The writer runs on its own thread, writing blocks from the queue.
